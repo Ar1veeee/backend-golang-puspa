56a626c52023f3cfd71a1a78419f0da55eb946a2
Ar1veeee <aliefarifin99@gmail.com>
2025-09-03 17:21:19 +0700

    fix(model): enforce NOT NULL and type constraint for user role column

--- shared/models/user.go
+++ shared/models/user.go
@@ -13,7 +13,7 @@ type User struct {
 	Username  string    `json:"username" gorm:"type:varchar(50);uniqueIndex;not null"`
 	Email     string    `json:"email" gorm:"type:varchar(50);uniqueIndex;not null"`
 	Password  string    `json:"password"`
-	Role      string    `json:"role"`
+	Role      string    `json:"role" gorm:"type:varchar(50);not null"`
 	CreatedAt time.Time `json:"createdAt" gorm:"column:createdAt;autoCreateTime"`
 	UpdatedAt time.Time `json:"updatedAt" gorm:"column:updatedAt;autoUpdateTime"`
 
